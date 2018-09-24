614642e5a84ad43daccde1f473ac2a6f1ff67ea1
Albert Yang <32423607+GNSPhenomena@users.noreply.github.com>
2018-09-24 14:57:07 -0700

    Create example.c

---
+++ example.c
@@ -0,0 +1,25 @@
+//pins for ultrasonic
+const int trigPin = ;
+const int echoPin = ;
+
+//pins for RGB LED
+const int redPin = ;
+const int greenPin = ;
+const int bluePin = ;
+
+void setup()
+{ 
+  //Initialize the Serial class
+  /* Your Code */
+  
+  //Initialize your ultrasonic pins
+  /* Your Code */
+ 
+  //initialize your RGB LED pins
+  /* Your Code */
+}
+
+void loop
+{
+  /* Your Code */
+}
